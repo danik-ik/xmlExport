623142b32d965b9ae24237ce9cd75d64a317bc72
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2016-08-13 12:58:17 +0300

    Готова канва приложения

--- XmlExport.dpr
+++ XmlExport.dpr
@@ -1,13 +1,6 @@
 program XmlExport;
 
 {$APPTYPE CONSOLE}
-
-uses
-  SysUtils,
-  Db2XML,
-  XMLDoc, XMLIntf, DB, AdoDb;
-
-begin
   (*
   Программа предназначена для конвертации таблицы БД (без связанных таблих
   с детализациями) в формат XML.
@@ -15,10 +8,84 @@ begin
     Поля запроса могут быть записаны в виде текстового значения ноды
     Поля запроса могут быть записаны в виде атрибута ноды
     Ноды могут вкладываться для группировки
-  
+
+  Соединение с базой данных: data.udl
+  Запрос: query.sql
+  Правила конвертации: convert.rules
+
+  Параметры командной строки:
+
+  Имя выходного файла: /file
+  Имя корневой ноды: /root
+  Параметры корневой ноды: /rootparams
+
   *)
+
+uses
+  SysUtils,
+  Classes,
+  Db2XML,
+  XMLDoc, XMLIntf, DB, AdoDb;
+
+var
+  OutFileName: string; // Имя выходного xml-файла
+  RootNodeName: string; // Имя корневой ноды
+  RootParans: string; // Параметры коневой ноды
+
+  QuerySQL: string; // SQL-запрос
+  Rules: string; // Правила конвертации
+
+const
+  CONNECTION_STRING = 'FILE NAME=.\data.udl';
+  QUERY_FILE_NAME = '.\query.sql';
+  RULES_FILE_NAME = '.\convert.rules';
+
+var
+  Connection: TADOConnection;
+  Query: TADOQuery;
+
+  procedure GetParameters;
+  var
+    TextReader: TStringList;
+  begin
+    try
+      TextReader := TStringList.Create;
+      ////
+    finally
+      TextReader.Free;
+    end;
+
+  end;
+
+procedure OpenDataBase;
+begin
+
+end;
+
+procedure CreateConvertor;
+begin
+
+end;
+
+procedure RunConvertor;
+begin
+
+end;
+
+
+begin
   // Глобальная обработка исключений: запись в лог, возвращаем 1
-  // Открываем базу данных
-  // Загружаем файл с правилами конвертации
-  // Создаём объекты TDb2XmlRoot, TDb2XmlDataLayer
+  try
+    GetParameters;
+    OpenDataBase;
+    CreateConvertor;
+    RunConvertor;
+  except
+    on e: Exception do
+    begin
+      WriteLn(ErrOutput, e.Message);
+      if ExitCode = 0 then
+        ExitCode := 1;
+    end;
+  end;
 end.
